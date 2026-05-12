1ba2843fec10da1ffdada4bcd426a1ad3cf1dcba
suja-shrestha <shresthasujal917@gmail.com>
2026-05-12 07:37:46 +0545

    Auto-save

--- pyy/conditional.py
+++ pyy/conditional.py
@@ -1,5 +1,11 @@
-provided_number = int(input("Enter a number to find composite number: "))
-print(f"You entered: {provided_number}")
+provided_number = int(input("Enter a number to check Composite or Prime: "))
 
-if 
-
+if provided_number <= 1:
+    print(f"{provided_number} is neither prime nor composite.")
+else:
+    for i in range(2, provided_number):
+        if provided_number % i == 0:
+            print(f"{provided_number} is a composite number.")
+            break
+    else:
+        print(f"{provided_number} is a prime number.")
